9318dae443cbe683bbbefa639556e94564b29e17
dailliwang <758017188@qq.com>
2018-01-16 15:27:33 +0800

    time and score

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 // 初始化Phaser，并创建一个和屏幕相同大小的场景
 var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
-var obstacle_velocity = 100;
-var	star_velocity = 100;
+// 障碍物和奖励的速度
+var obstacle_velocity = 150;
+var	star_velocity = 150;
+// x滑动的最小触发距离
+var minTouchDis = screenWidth / 3;
 var game = new Phaser.Game(screenWidth, screenHeight, Phaser.CANVAS, 'game_div');
 var game_state = {};
 
@@ -45,10 +48,8 @@ game_state.main.prototype = {
         // 创建一个group，包含10个奖励星星
         this.stars = game.add.group();
         this.stars.enableBody = true;
-        this.stars.createMultiple(10, 'star');
+        this.stars.createMultiple(2, 'star');
        
-        // 定时器，创建障碍物
-        this.timer = this.game.time.events.loop(1000, this.add_obstacles, this); 
         // 触摸按下的开始x坐标
         this.startX = 0;
         // 监听按下事件
@@ -63,9 +64,9 @@ game_state.main.prototype = {
 			console.log('endXStartx-->'+this.startX);
 			console.log('endX-->'+pointer.x);
 			console.log('offset-->'+(pointer.x - this.startX))
-			if(pointer.x - this.startX>100){
+			if(pointer.x - this.startX > minTouchDis){
 				this.moveCar('right');
-			}else if(pointer.x - this.startX<-100){
+			}else if(pointer.x - this.startX< -minTouchDis){
 				this.moveCar('left');
 			}
 			this.startX = 0;
@@ -75,11 +76,23 @@ game_state.main.prototype = {
 			console.log(3);
 		},this);
 		
+		// 添加时间
+		this.remainTime = 30;
+        var style = { font: "20px Arial", fill: "#ffffff" };
+        this.remainTimeText = this.game.add.text(20, 20, "时间: "+this.remainTime, style);
+		
 		// 添加分数
 		this.score = 0;
-        this.scoreText = "分数: "+this.score;
-        var style = { font: "30px Arial", fill: "#ffffff" };
-        this.scoreText = this.game.add.text(20, 20, "分数: 0", style);  
+        var style = { font: "20px Arial", fill: "#ffffff" };
+        this.scoreText = this.game.add.text(20, 20, "分数: "+this.score, style);
+        this.scoreText.x = game.world.width - this.scoreText.width - this.scoreText.width;
+        
+        // 定时器，创建障碍物和奖励
+        this.timer = this.game.time.events.loop(2000, this.add_move_sprite, this); 
+        // 定时器，减少时间
+        this.reduceTimer = this.game.time.events.loop(1000, this.reduceTime, this); 
+        // 定时器，实时增加分数
+        this.increaseScoreTimer = this.game.time.events.loop(1, this.increaseScore, this); 
     },
     update: function() {
     	// 小车和障碍物的碰撞监听
@@ -144,7 +157,8 @@ game_state.main.prototype = {
         }
        
     },
-    add_obstacles: function(){
+    add_move_sprite: function(){
+    	console.log("timere")
     	// 随机[0,1]的整数，确定是创建障碍物还是星星
     	var starNum = Math.floor(Math.random()*2);
     	if(starNum === 0){
@@ -153,25 +167,63 @@ game_state.main.prototype = {
     		this.add_one_obstacle();
     	}
     },
+    reduceTime: function(){
+    	--this.remainTime;
+        this.remainTimeText.text = "时间: "+this.remainTime;
+        // 结束场景
+        if(this.remainTime <= 0){ 
+        	// 移除定时器
+        	this.game.time.events.remove(this.timer);
+        	this.game.time.events.remove(this.reduceTimer);
+        	this.game.time.events.remove(this.increaseScoreTimer);
+        	game.state.add('stop', game_state.stop);
+			game.state.start('stop'); 
+        }
+    },
+    increaseScore: function(){
+    	// 增加分数
+    	this.score += 1;
+        this.scoreText.text = '分数: ' + this.score; 
+    },
     collectCarFunc: function(car, obstacle){
     	obstacle.kill();
     	console.log("碰到了障碍物+减分");
-    	this.score -= 1;
-    	if(this.score < 0){
-    		this.score = 0;
-    	}
-        this.scoreText.text = '分数: ' + this.score;  
-         this.car.animations.play('right');
+    	this.car.animations.play('right');
+    	
+        // 减少时间
+        this.remainTime -= 5;
+        if(this.remainTime < 0){
+        	this.remainTime = 0;
+        }
+        this.remainTimeText.text = "时间: "+this.remainTime;
     },
     eatStarFunc: function(car, star){
     	star.kill();
     	console.log("吃到奖励+加分");
-    	
-    	this.score += 1;
-        this.scoreText.text = '分数: ' + this.score; 
         this.car.animations.play('left');
+        
+        // 增加时间
+        this.remainTime += 5;
+        this.remainTimeText.text = "时间: "+this.remainTime;
     }
 };
+
+
+// 创建一个结束场景
+game_state.stop = function() { };
+game_state.stop.prototype = {
+	preload: function(){
+		this.game.stage.backgroundColor = '#0000cf';
+	},
+	create: function(){
+		var style = { font: "30px Arial", fill: "#ffffff" };
+		this.stopText = this.game.add.text(game.world.width/2, game.world.height/2, "游戏结束", style);  
+		this.stopText.x = game.world.width/2 - this.stopText.width/2;
+	},
+	update: function(){
+		
+	}
+}
 
 game.state.add('main', game_state.main);  
 game.state.start('main'); 
